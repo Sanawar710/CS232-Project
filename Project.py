6b07b7d5000e879a99474faba5d45e95190a44db
Sanawar <abrarkhanmohammadsanawar@gmail.com>
2025-04-12 19:23:30 +0500

    Refactor database table creation scripts for clarity and consistency; add missing foreign key constraints and improve comments.

--- Project.py
+++ Project.py
@@ -1,7 +1,7 @@
 import psycopg2 as pg  # 'psycopg2' is used to interact with the PostgreSQL database
 import pandas as pd  # Pandas is a data manipulation and analysis library
 from flask import Flask  # Flask is a high-level Python web framework
-from flask import render_template # 'render_template' is used to render HTML files
+from flask import render_template  # 'render_template' is used to render HTML files
 
 df = pd.DataFrame()  # Global DataFrame to hold student data
 
@@ -12,6 +12,7 @@ def Absoulte_Grading():
     It assigns grades based on the specified ranges for each grade.
     """
     global df
+
 
 try:
     df["Grade"] = pd.cut(
@@ -57,17 +58,17 @@ def Relative_Grading():
         pass
 
 
-# def authenticate(
-#     name, password
-# ):  # Authenticate function to verify username and password
-#     '''
-#     Args:
-#         name (str): The username of the user.
-#         password (str): The password of the user.
-#     Returns:
-#         bool: True if the username is "admin" and the password is "ABC", else False.
-#     '''
-#     return name == "admin" and password == "ABC"
+def authenticate(
+    name, password
+):  # Authenticate function to verify username and password
+    '''
+    Args:
+        name (str): The username of the user.
+        password (str): The password of the user.
+    Returns:
+        bool: True if the username is "admin" and the password is "ABC", else False.
+    '''
+    return name == "admin" and password == "ABC"
 
 
 # # Later to be modified, just a basic implementation of Admin's Class
@@ -112,58 +113,60 @@ try:
 
     print("Connected to the database")
 
-    # Previous Tables to be Implemented till now
-    createtable_script = '''CREATE TABLE Users (
+    # User Table
+    user_script = """CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL CHECK (role IN ('student', 'instructor', 'admin')),
     profile_pic VARCHAR(255)
-);'''
-    cursor.execute(createtable_script)
+);"""
+    cursor.execute(user_script)
     cursor.commit()
-    
-    createtable_script = '''
-    CREATE TABLE Courses (
+
+    # Courses Table
+    courses_script = """CREATE TABLE Courses (
     course_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     credit_hours INT NOT NULL CHECK (credit_hours BETWEEN 1 AND 4),
     instructor_id INT,
     semester VARCHAR(20),
     FOREIGN KEY (instructor_id) REFERENCES Users(user_id)
-);'''
-    cursor.execute(createtable_script)
+);"""
+    cursor.execute(courses_script)
     cursor.commit()
 
-    createtable_script = '''
-    CREATE TABLE CoursePrerequisites (
+    # Course Prerequisites
+    course_prerequisite_script = """CREATE TABLE CoursePrerequisites (
     course_id INT,
     prerequisite_id INT,
     PRIMARY KEY (course_id, prerequisite_id),
     FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE CASCADE,
     FOREIGN KEY (prerequisite_id) REFERENCES Courses(course_id) ON DELETE CASCADE
-);
-    '''
-    cursor.execute(createtable_script)
+);"""
+    cursor.execute(course_prerequisite_script)
     cursor.commit()
 
-    createtable_script = '''
-    CREATE TABLE Registrations (
+    # Registrations
+    registration_script = """CREATE TABLE Registrations (
     registration_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     course_id INT NOT NULL,
-    status VARCHAR(20) CHECK (status IN ('enrolled', 'completed', 'dropped')) DEFAULT 'enrolled',
+    status VARCHAR(20) DEFAULT 'enrolled',
     semester VARCHAR(20),
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE CASCADE
-);'''
-    cursor.execute(createtable_script)
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE CASCADE,
+    CHECK (
+        (semester = '1' AND status = 'enrolled') OR
+        (semester <> '1' AND status IN ('enrolled', 'completed', 'dropped'))
+    )
+);"""
+    cursor.execute(registration_script)
     cursor.commit()
 
-    createtable_script = '''
- 
-    CREATE TABLE Results (
+    # Results
+    result_script = """CREATE TABLE Results (
     result_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     course_id INT NOT NULL,
@@ -175,13 +178,12 @@ try:
     grade VARCHAR(2),
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE CASCADE
-); '''
-    cursor.execute(createtable_script)
+);"""
+    cursor.execute(result_script)
     cursor.commit()
 
-
-    createtable_script = '''
-    CREATE TABLE Attendance (
+    # Attendance
+    attendance_script = """CREATE TABLE Attendance (
     attendance_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     course_id INT NOT NULL,
@@ -189,81 +191,73 @@ try:
     status VARCHAR(10) CHECK (status IN ('present', 'absent', 'late')) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE CASCADE
-);'''
-    cursor.execute(createtable_script)
-    cursor.commit() 
-
-    attendance_script = """
-    CREATE TABLE IF NOT EXISTS attendance (
-        id SERIAL PRIMARY KEY,
-        user_id INT,
-        course_id INT,
-        date DATE NOT NULL,
-        status VARCHAR(10) CHECK (status IN ('absent', 'present', 'leave')) NOT NULL, 
-        FOREIGN KEY (user_id) REFERENCES Users(id),
-        FOREIGN KEY (course_id) REFERENCES courses(course_id)
-    );
-    """
+);"""
     cursor.execute(attendance_script)
-    conn.commit()
-
-    message_script = """
-    CREATE TABLE IF NOT EXISTS message(
-       Message_id SERIAL PRIMARY KEY,
-       FOREIGN KEY (sender_id) REFERENCES Users(id),
-       FOREIGN KEY (receiver_id) REFERENCES Users(id),
-       Message TEXT,
-       Status VARCHAR(100),
-       Time TIMESTAMP
-       );"""
-    cursor.execute(message_script)
-    conn.commit()
-
-    bug_script = """
-    CREATE TABLE IF NOT EXISTS bug(
-        bug_id SERIAL PRIMARY KEY,
-        FOREIGN KEY (sender_id) REFERENCES Users(id),
-        Description TEXT NOT NULL,
-        status VARCHAR(10) CHECK (status IN ('open', 'in_progress', 'closed')) NOT NULL,
-        Time TIMESTAMP
-        );
-   """
-    cursor.execute(bug_script)
-    conn.commit()
-
-    rechcecking_script = """
-    CREATE TABLE IF NOT EXISTS rechecking(
-        recheck_id SERIAL PRIMARY KEY,
-        FOREIGN KEY (sender_id) REFERENCES Users(id),
-        FOREIGN KEY (course_id) REFERENCES Courses(id),
-        reason TEXT NOT NULL,
-        exam_type VARCHAR(10) CHECK (exam_type IN ('quiz', 'mid term', 'final')) NOT NULL,
-        status VARCHAR(10) CHECK (status IN ('pending', 'approved', 'rejected')) NOT NULL,
-        );
-    """
-    cursor.execute(rechcecking_script)
-    conn.commit()
-
-    academic_script = """
-    CREATE TABLE IF NOT EXISTS academic_calendar(
-        event_id SERIAL PRIMARY KEY,
-        event_name VARCHAR(100) NOT NULL,
-        description TEXT NOT NULL,
-        event_date DATE);
-"""
-    cursor.execute(academic_script)
     cursor.commit()
 
-    feedback_script = """
-    CREATE TABLE IF NOT EXISTS feedback(
-       feedback_id SERIAL PRIMARY KEY,
-       FOREIGN KEY (sender_id) REFERENCES Users(id),
-       FOREIGN KEY (course_id) REFERENCES Courses(id),
-       FOREIGN KEY (instructor_id) REFERENCES Users(id),
-       rating INT CHECK (rating BETWEEN 1 AND 5),
-       comments TEXT,
-       time TIMESTAMP
-       );"""
+    # Messages
+    message_script = """CREATE TABLE IF NOT EXISTS message (
+    Message_id SERIAL PRIMARY KEY,
+    sender_id INT,
+    receiver_id INT,
+    Message TEXT,
+    Status VARCHAR(100),
+    Time TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES Users(user_id),
+    FOREIGN KEY (receiver_id) REFERENCES Users(user_id)
+);"""
+    cursor.execute(message_script)
+    cursor.commit()
+
+    # Bugs
+    bugs_script = """CREATE TABLE IF NOT EXISTS bug (
+    bug_id SERIAL PRIMARY KEY,
+    sender_id INT,
+    Description TEXT NOT NULL,
+    status VARCHAR(10) CHECK (status IN ('open', 'in_progress', 'closed')) NOT NULL,
+    Time TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES Users(user_id)
+);"""
+    cursor.execute(bugs_script)
+    cursor.commit()
+
+    # Rechecking
+    rechecking_script = """CREATE TABLE IF NOT EXISTS rechecking (
+    recheck_id SERIAL PRIMARY KEY,
+    sender_id INT,
+    course_id INT,
+    reason TEXT NOT NULL,
+    exam_type VARCHAR(10) CHECK (exam_type IN ('quiz', 'mid term', 'final')) NOT NULL,
+    status VARCHAR(10) CHECK (status IN ('pending', 'approved', 'rejected')) NOT NULL,
+    FOREIGN KEY (sender_id) REFERENCES Users(user_id),
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+);"""
+    cursor.execute(rechecking_script)
+    cursor.commit()
+
+    # Academic Calendar
+    calendar_script = """CREATE TABLE IF NOT EXISTS academic_calendar (
+    event_id SERIAL PRIMARY KEY,
+    event_name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    event_date DATE
+);"""
+    cursor.execute(calendar_script)
+    cursor.commit()
+
+    # Feedback
+    feedback_script = """CREATE TABLE IF NOT EXISTS feedback (
+    feedback_id SERIAL PRIMARY KEY,
+    sender_id INT,
+    course_id INT,
+    instructor_id INT,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comments TEXT,
+    time TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES Users(user_id),
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
+    FOREIGN KEY (instructor_id) REFERENCES Users(user_id)
+);"""
     cursor.execute(feedback_script)
     cursor.commit()
 
@@ -271,7 +265,7 @@ except Exception as e:
     print("Error:", e)
     print("Failed to connect to the database")
 
-finally:
+finally:  # Will close the connection and cursor if they were created. It will be executed no matter what.
     if cursor is not None:
         cursor.close()
     if conn is not None:
